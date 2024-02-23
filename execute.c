e93262aaafac3f86190922ecc46f53b614c342ac
Brenty-Boy <125645093+Brenty-Boy@users.noreply.github.com>
2024-02-24 01:02:15 +0200

    Main

--- execute.c
+++ execute.c
@@ -3,7 +3,7 @@
 * execute - executes the opcode
 * @stack: head linked list - stack
 * @counter: line_counter
-* @file: poiner to monty file
+* @file: pointer to monty file
 * @content: line content
 * Return: no return
 */
